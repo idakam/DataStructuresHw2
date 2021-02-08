cb062e5f3d5d1854b63507e528612a14a2456917
idakam <77304879+idakam@users.noreply.github.com>
2021-02-08 09:13:43 -0600

    Update ArrayQuestion.java

--- src/datastructureshw/ArrayQuestion.java
+++ src/datastructureshw/ArrayQuestion.java
@@ -2,12 +2,12 @@ package datastructureshw;
 
 public class ArrayQuestion {
 	//Write program to sum all values of given Array
-		public static int arraySum(int[] numbers){
-			int mySum = 0;
-			for(int i=0; i < numbers.length; i++) {
-				mySum += numbers[i];
-			}
-			return mySum;
+	public static int arraySum(int[] numbers){
+		int mySum = 0;
+		for(int i=0; i < numbers.length; i++) {
+			mySum += numbers[i];
 		}
+		return mySum;
+	}
 		
 }
